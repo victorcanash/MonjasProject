ff758d0c50aa2614f0e27ac5a94ea31614f45f82
VictorCanas <victorcanas972@gmail.com>
2019-12-31 14:01:36 +0100

    SplashScreen and LoadScreen

---
+++ Assets/_Scripts/Scenes/SplashScreenController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace MonjasProject.Scenes
+{
+    public class SplashScreenController : MonoBehaviour
+    {
+        private void Start()
+        {
+            Invoke("LoadScene", 2f);
+        }
+
+        private void LoadScene()
+        {
+            ScenesManager.Instance.LoadScene("MainMenu");
+        }
+    }
+}
